3580c2419ce90d40adbfd2209099e781a2895c07
Gautam Doddamani <gautamad.dodd@gmail.com>
2019-04-24 21:07:15 +0530

    heroku build

---
+++ build/precache-manifest.88cf7397c025918246d876114c891fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74305c72574918427c2706a9fe71b34a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fcdcc6e64655193a2ae",
+    "url": "/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "0c4d4f2a0f6aa7fc5987",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "6fcdcc6e64655193a2ae",
+    "url": "/static/js/2.f493f5f8.chunk.js"
+  },
+  {
+    "revision": "0c4d4f2a0f6aa7fc5987",
+    "url": "/static/js/main.f5b471ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
